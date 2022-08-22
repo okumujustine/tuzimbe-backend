6458d2d8d750cc36b72696ae82f274cf7b297564
Okumu Justine <okumujustine01@gmail.com>
2022-08-22 14:11:25 +0300

    add string representation to classes

--- materials/admin.py
+++ materials/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from .models import Material
+from .models import (
+    Material,
+    MeasurementMethod,
+)
 
 admin.site.register(Material)
 admin.site.register(MeasurementMethod)
--- materials/models.py
+++ materials/models.py
@@ -7,8 +7,14 @@ class MeasurementMethod(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Material(models.Model):
     name = models.CharField(max_length=100)
     mesurements = models.ManyToManyField(MeasurementMethod, related_name="material_mesurement_methods")
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
