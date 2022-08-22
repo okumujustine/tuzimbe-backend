e05a745bb738c8a9b4c03ac9fe80650410777005
Okumu Justine <okumujustine01@gmail.com>
2022-08-22 12:30:27 +0300

    add worker endpoint setup

--- workers/views.py
+++ workers/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def get_all_workers(request):
+    return Response({"name":"okumu justine"})
