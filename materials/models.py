f42a7019bb3395fff411a2cdc6871544e7aded85
Okumu Justine <okumujustine01@gmail.com>
2022-08-22 14:05:01 +0300

    add mesurement method and material model

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class MeasurementMethod(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+    mesurements = models.ManyToManyField(MeasurementMethod, related_name="material_mesurement_methods")
+    created_at = models.DateTimeField(auto_now_add=True)
