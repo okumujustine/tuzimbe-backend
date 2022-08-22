3922a2858589b1c5b1ee710eecd90c3fd98771fd
Okumu Justine <okumujustine01@gmail.com>
2022-08-22 13:40:34 +0300

    add worker serializer and view logic

--- workers/views.py
+++ workers/views.py
@@ -2,7 +2,14 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .models import Worker
+from .serializers import (
+    WokerSerializer,
+)
+
 
 @api_view(['GET'])
 def get_all_workers(request):
-    return Response({"name":"okumu justine"})
+    workers = Worker.objects.all()
+    serializerd_workers = WorkerSerializer(workers, many=True)
+    return Response(serializerd_workers.data)
